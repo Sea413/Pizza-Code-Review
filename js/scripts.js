20b64384237c99e863fec458eedfb72fa1027f0e
sea413 <student@epicodus.com>
2016-01-29 11:01:14 -0800

    create a resolution for the existing spec error. Specs will be expanded to accomodate the existing demands of the program.

--- js/scripts.js
+++ js/scripts.js
@@ -5,27 +5,30 @@ function Customer(name,psize,topping) {
 }
 
 customer.prototype.sizeselect = function(){
-    return  this.psize = psize;
+    this.psize = psize;
+    this.topping = topping;
     // this.currentscore = new_array;
  }
- customer.prototype.toppingselect = function(){
-     return  this.topping = topping;
-     // this.currentscore = new_array;
-  }
+ // customer.prototype.toppingselect = function(){
+ //     return  this.topping = topping;
+ //     // this.currentscore = new_array;
+ //  }
 $(document).ready(function() {
   $("form#question").submit(function(event) {
     event.preventDefault();
     var nameentry = $("input#customer").val();
     var newcust = new Customer(nameentry);
 
-    $("form#pizzasize").submit(function(event) {
-      event.preventDefault();
+    $("form#size").submit(function(event) {
+      // event.preventDefault();
       var vsize = $("select#pizzasize").val();
-      newcust.sizeselect(nameentry,vsize);
-      // $("form#toppings").submit(function(event) {
-      //   event.preventDefault();
-      //   var vtopping = $("select#pizzatoppings").val();
-      //   newcust.toppingselect(nameentry,vsize);
+      newcust.sizeselect(nameentry,vsize,vtopping);
+
+      $("form#toppings").submit(function(event) {
+        var nameentry = $("input#customer").val();
+        var vtopping = $("select#pizzatoppings").val();
+        newcust.sizeselect(nameentry,vsize,vtopping);
+        });
     });
 
   });
