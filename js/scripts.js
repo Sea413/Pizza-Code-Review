e72712e10bf32c19534371bc88d1b3db0caa2a1f
sea413 <student@epicodus.com>
2016-01-29 10:38:35 -0800

    Offending code has been located and further testing in constructor information will be required.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-function Customer(name, psize,ptopping) {
+function Customer(name,psize,topping) {
   this.name = name;
   this.psize = psize;
-  this.ptopping = ptopping;
+  this.topping = topping;
 }
 
 customer.prototype.sizeselect = function(){
     return  this.psize = psize;
+    // this.currentscore = new_array;
  }
- customer.prototype.toppingselect = function()
- {
-     return  this.ptopping = ptopping;
+ customer.prototype.toppingselect = function(){
+     return  this.topping = topping;
+     // this.currentscore = new_array;
   }
 $(document).ready(function() {
   $("form#question").submit(function(event) {
@@ -17,17 +18,16 @@ $(document).ready(function() {
     var nameentry = $("input#customer").val();
     var newcust = new Customer(nameentry);
 
-    $("form#roll").submit(function(event) {
+    $("form#pizzasize").submit(function(event) {
       event.preventDefault();
       var vsize = $("select#pizzasize").val();
       newcust.sizeselect(nameentry,vsize);
-        $("form#toppings").submit(function(event) {
-          event.preventDefault();
-          var vtopping = $("select#pizzatoppings").val();
-          newcust.toppingselect(nameentry,vsize);
+      // $("form#toppings").submit(function(event) {
+      //   event.preventDefault();
+      //   var vtopping = $("select#pizzatoppings").val();
+      //   newcust.toppingselect(nameentry,vsize);
     });
 
   });
 
 });
-// });
