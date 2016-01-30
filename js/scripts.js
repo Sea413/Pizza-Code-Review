13cb0292b9b82439c98cf6bda97e60a794063e93
sea413 <student@epicodus.com>
2016-01-29 16:26:18 -0800

    Prepare for remote branch push.

--- js/scripts.js
+++ js/scripts.js
@@ -7,95 +7,93 @@ function Customer(name,psize,topping,currentprice,cost) {
 }
 
 Customer.prototype.sizeselect = function(vsize){
-    if (vsize === "Trifling") {
-      this.psize === vsize;
-      this.cost += 5;
-      return this.cost;
-    }
-    else if (vsize === "Picayune"){
-      this.psize === vsize;
-      this.cost += 8;
-      return this.cost;
-    }
-    else if (vsize === "Abhorrent"){
-      this.psize === vsize;
-      this.cost += 12;
-      return this.cost;
-    }
-    else if (vsize === "Brobdingnagian"){
-      this.psize === vsize;
-      this.cost += 15;
-      return this.cost;
-      }
-    }
-//  }
+  if (vsize === "Trifling") {
+    this.psize === vsize;
+    this.cost += 5;
+    return this.cost;
+  }
+  else if (vsize === "Picayune"){
+    this.psize === vsize;
+    this.cost += 8;
+    return this.cost;
+  }
+  else if (vsize === "Abhorrent"){
+    this.psize === vsize;
+    this.cost += 12;
+    return this.cost;
+  }
+  else if (vsize === "Brobdingnagian"){
+    this.psize === vsize;
+    this.cost += 15;
+    return this.cost;
+  }
+}
+
  Customer.prototype.toppingselect = function(vtopping){
-   if (vtopping === "atari") {
+   if (vtopping === "Atari") {
      this.currentprice += 15;
      this.topping === vtopping;
      this.cost += 15;
      return this.cost;
    }
-   else if (vtopping === "tears"){
+   else if (vtopping === "Tears"){
      this.currentprice += 28;
      this.topping === vtopping;
      this.cost += 28;
      return this.cost;
    }
-   else if (vtopping === "bento"){
+   else if (vtopping === "Bento"){
      this.currentprice += 32;
      this.topping === vtopping;
      this.cost += 32;
      return this.cost;
    }
-   else if (vtopping === "cheese"){
+   else if (vtopping === "Cheese"){
      this.currentprice += 45;
      this.topping === vtopping;
      this.cost += 45;
      return this.cost;
-} else if (vtopping === "choice"){
-  this.currentprice += 1000;
-  this.topping === vtopping;
-  this.cost += 1000;
-  return this.cost;
-} else if (vtopping === "kale"){
-  this.currentprice += 55;
-   this.topping === vtopping;
-   this.cost += 55;
-   return this.cost;
+  }
+  else if (vtopping === "Choice"){
+    this.currentprice += 1000;
+    this.topping === vtopping;
+    this.cost += 1000;
+    return this.cost;
+  }
+  else if (vtopping === "Kale"){
+    this.currentprice += 55;
+     this.topping === vtopping;
+     this.cost += 55;
+     return this.cost;
+   }
  }
- }
- console.log(this.cost);
 $(document).ready(function() {
   $("form#question").submit(function(event) {
     event.preventDefault();
     var nameentry = $("input#customer").val();
     var newcust = new Customer(nameentry);
-     $("#customern").append(nameentry);
-     $("#size").show();
-        $("form#size").submit(function(event) {
-          event.preventDefault();
-          var vsize = $("select#pizzasize").val();
-          newcust.sizeselect(vsize);
-          $("#pizzat").append(vsize + " " + "$" + newcust.cost);
-           $("#toppings").show();
-          $("form#toppings").submit(function(event) {
-            event.preventDefault();
-            var nameentry = $("input#customer").val();
-            var vtopping = $("select#pizzatoppings").val();
-            newcust.toppingselect(vtopping);
-            $("#toppingt").append( " " + vtopping + " " + "$" + newcust.currentprice);
-            $("#confirmation").show();
-            $("form#confirmation").submit(function(event) {
-              event.preventDefault();
-            $("#finalcost").html("$" + newcust.cost);
-            $("#winning").show();
+    $("#customern").append(nameentry);
+    $("#size").show();
+  $("form#size").submit(function(event) {
+    event.preventDefault();
+    var vsize = $("select#pizzasize").val();
+    newcust.sizeselect(vsize);
+    $("#pizzat").append(vsize + " " + "$" + newcust.cost);
+    $("#toppings").show();
+  $("form#toppings").submit(function(event) {
+    event.preventDefault();
+    var nameentry = $("input#customer").val();
+    var vtopping = $("select#pizzatoppings").val();
+    newcust.toppingselect(vtopping);
+    $("#toppingt").append( " " + vtopping + " " + "$" + newcust.currentprice);
+    $("#confirmation").show();
+  $("form#confirmation").submit(function(event) {
+    event.preventDefault();
+    $("#finalcost").html("$" + newcust.cost);
+    $("#winning").show();
 
-          });
-        });
+     });
     });
-
-  });
-
+   });
+ });
 });
-// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,9 @@
 describe("Customer", function(){
   it("Defines the current customer name", function(){
-  var testObject = new Customer ("Jerry","Trifling","kale");
+  var testObject = new Customer ("Jerry","Trifling","Kale");
   expect(testObject.name).to.equal("Jerry");
   expect(testObject.psize).to.equal("Trifling");
-  expect(testObject.topping).to.equal("kale");
+  expect(testObject.topping).to.equal("Kale");
 });
   it("Expands the definition of customer to include the size of the pizza", function(){
     var testObject = new Customer ("Jerry","Trifling");
@@ -11,29 +11,29 @@ describe("Customer", function(){
     expect(testObject.psize).to.equal("Trifling");
 });
   it("continue the expansion of the customer instructor to include topping size", function(){
-    var testObject = new Customer ("Jerry","Trifling","kale");
+    var testObject = new Customer ("Jerry","Trifling","Kale");
     expect(testObject.name).to.equal("Jerry");
     expect(testObject.psize).to.equal("Trifling");
-    expect(testObject.topping).to.equal("kale");
+    expect(testObject.topping).to.equal("Kale");
 });
   it("will begin to include pricing as a potential variable", function(){
-    var testObject = new Customer ("Jerry","Trifling","kale", 0);
+    var testObject = new Customer ("Jerry","Trifling","Kale", 0);
     expect(testObject.name).to.equal("Jerry");
     expect(testObject.psize).to.equal("Trifling");
-    expect(testObject.topping).to.equal("kale");
+    expect(testObject.topping).to.equal("Kale");
     expect(testObject.cost).to.equal(0);
 });
   it("will begin create parameters for pricing.", function(){
-    var testObject = new Customer ("Jerry","Trifling","kale", 0);
+    var testObject = new Customer ("Jerry","Trifling","Kale", 0);
     expect(testObject.sizeselect("Abhorrent")).to.equal(12);
 });
   it("will further expand upon pricing parameters to include toppings.", function(){
-    var testObject = new Customer ("Jerry","Abhorrent","kale", 0);
-    expect(testObject.toppingselect("kale")).to.equal(55);
+    var testObject = new Customer ("Jerry","Abhorrent","Kale", 0);
+    expect(testObject.toppingselect("Kale")).to.equal(55);
 });
   it("will finalize pricing options.", function(){
-    var testObject = new Customer ("Jerry","Abhorrent","kale", 0);
+    var testObject = new Customer ("Jerry","Abhorrent","Kale", 0);
     expect(testObject.sizeselect("Abhorrent")).to.equal(12);
-    expect(testObject.toppingselect("kale")).to.equal(67);
+    expect(testObject.toppingselect("Kale")).to.equal(67);
 });
 });
